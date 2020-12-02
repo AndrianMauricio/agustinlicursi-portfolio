5ed15d29b308a52d714323547f51e9a5571fdf5c
Mauricio Andrian <andrian.mauricio@gmail.com>
2020-12-02 20:41:17 -0300

    Added images to Airbnb

--- pages/caso-de-estudio-airbnb.tsx
+++ pages/caso-de-estudio-airbnb.tsx
@@ -1,12 +1,13 @@
 import { Body } from 'components/Body'
 import { HeaderBanner } from 'components/HeaderBanner'
-import { Fragment } from 'react'
+import { Fragment, ReactNode } from 'react'
 import { Card } from 'components/Card'
 import { TextSection, ItemsSection } from 'components/Section'
 import Head from 'next/head'
 import { Images } from 'components/Images'
 import { Content } from 'components/Content'
 import { Heading2 } from 'components/Heading2'
+import cn from 'classnames'
 
 export default function AirbnbStudyCase() {
   return (
@@ -52,8 +53,16 @@ export default function AirbnbStudyCase() {
           </p>
         </Content>
         <Images className="grid-cols-2" height="36rem">
-          <div className="bg-gray-500 rounded-lg shadow-lg  ml-28"></div>
-          <div className="bg-gray-500 rounded-lg shadow-lg  mr-28"></div>
+          <Picture
+            src="/images/airbnb/principio-de-proximidad.png"
+            title="PRINCIPIO DE PROXIMIDAD"
+            className="ml-28"
+          />
+          <Picture
+            src="/images/airbnb/principio-de-figura-y-fondo.png"
+            title="PRINCIPIO DE FONDO Y FIGURA"
+            className="mr-28"
+          />
         </Images>
 
         <Heading2>USABILIDAD Y REGLAS HEURÍSTICAS</Heading2>
@@ -64,8 +73,24 @@ export default function AirbnbStudyCase() {
             cumplimiento de las mismas.
           </p>
         </Content>
-        <Images className="grid-cols-1">
-          <div className="bg-gray-500 rounded-lg shadow-lg mx-28"></div>
+        <Images className="grid-cols-2 grid-rows-2">
+          <Picture
+            title="HABLAR EL LENGUAJE DEL USUARIO"
+            src="/images/airbnb/lenguaje-de-los-usuarios.png"
+            className="row-span-2 ml-28"
+          />
+
+          <Picture
+            title="VISIBILIDAD DE ESTADO DEL SISTEMA"
+            src="/images/airbnb/visibilidad-de-estado.png"
+            className="mr-28"
+          />
+
+          <Picture
+            title="Ayuda y documentación"
+            src="/images/airbnb/ayuday-doc.png"
+            className="mr-28"
+          />
         </Images>
 
         <Heading2>ARQUITECTURA DE LA INFORMACIÓN</Heading2>
@@ -79,9 +104,9 @@ export default function AirbnbStudyCase() {
             Card Sorting híbrido valiéndome de la herramienta Optimal Workshop.
           </p>
         </Content>
-        <Images className="grid-cols-2" height="20rem">
-          <div className="bg-gray-500 rounded-lg shadow-lg  ml-28"></div>
-          <div className="bg-gray-500 rounded-lg shadow-lg  mr-28"></div>
+        <Images className="grid-cols-2" height="16rem">
+          <Picture src="/images/airbnb/cardsorting01.png" className="ml-28" />
+          <Picture src="/images/airbnb/cardsorting02.png" className="mr-28" />
         </Images>
 
         <Heading2>PERSONAS</Heading2>
@@ -94,10 +119,13 @@ export default function AirbnbStudyCase() {
             usuarios creé 3 personas englobando 3 tipos de usuarios.
           </p>
         </Content>
-        <Images className="grid-cols-2 grid-rows-3" height="60rem">
-          <div className="bg-gray-500 rounded-lg shadow-lg  ml-28"></div>
-          <div className="bg-gray-500 rounded-lg shadow-lg  mr-28"></div>
-          <div className="row-start-2 row-end-4 col-start-1 col-end-3 bg-gray-500 rounded-lg shadow-lg mx-28"></div>
+        <Images className="grid-cols-2 grid-rows-3" height="55rem">
+          <Picture src="/images/airbnb/persona-02.svg" className="" />
+          <Picture src="/images/airbnb/persona-03.svg" className="" />
+          <Picture
+            src="/images/airbnb/persona-01.svg"
+            className="row-span-2 col-span-2"
+          />
         </Images>
 
         <Heading2>CUSTOMER JOURNEY MAP</Heading2>
@@ -109,10 +137,41 @@ export default function AirbnbStudyCase() {
             coordinación de la entrega de las llaves.
           </p>
         </Content>
-        <Images height="40rem">
-          <div className="bg-gray-500 rounded-lg shadow-lg mx-28"></div>
+        <Images height="32rem">
+          <Picture src="/images/airbnb/journeymap.png" className="mx-28" />
         </Images>
       </Body>
     </Fragment>
+  )
+}
+
+interface PictureProps {
+  src?: string
+  title?: string
+  className?: string
+}
+
+function Picture({ src, title, className }: PictureProps) {
+  return (
+    <div
+      className={cn(
+        'bg-gray-500 rounded-lg shadow-lg overflow-hidden relative',
+        className,
+      )}
+    >
+      {title && (
+        <div
+          className={cn(
+            'heading text-white uppercase font-medium',
+            'flex items-center justify-start',
+            'p-10',
+            'bg-blue-500 bg-opacity-90 absolute w-full h-20 bottom-0',
+          )}
+        >
+          {title}
+        </div>
+      )}
+      <img className="object-cover w-full h-full" src={src} />
+    </div>
   )
 }
